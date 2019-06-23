4c20b8c6ed95bbda33ef71aca3ee85f6e7b0dd32
俊祥 <909015430@qq.com>
2019-06-24 00:02:34 +0800

    搭建基本框架

--- JXWeibo/Classes/Main/MainViewController.swift
+++ JXWeibo/Classes/Main/MainViewController.swift
@@ -12,8 +12,28 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // 1.创建首页
+        tabBar.tintColor = .orange
+        addChild(HomeTableViewController(), "首页", imageName: "tabbar_home")
+        addChild(MessageTableViewController(), "消息", imageName: "tabbar_message_center")
+        addChild(DiscoverTableViewController(), "发现", imageName: "tabbar_discover")
+        addChild(ProfileTableViewController(), "我", imageName: "tabbar_profile")
+    }
+    
+    /**
+     初始化自控制
+     */
+    private func addChild(_ childController: UIViewController,_ title:String, imageName:String) {
+        // 1 设置首页tabbar 对应的数据
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        childController.title = title
+        
+        // 2.给首页包装导航控制器
+        let nav = UINavigationController()
+        nav.addChild(childController)
+        // 3.add
+        addChild(nav)
     }
 
 }
