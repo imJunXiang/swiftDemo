d1d2d1a7fb4ad5ad73d54c160fe6c67d278f33a5
俊祥 <909015430@qq.com>
2019-06-24 20:32:47 +0800

    动态加载命名空间

--- JXWeibo/Classes/Main/MainViewController.swift
+++ JXWeibo/Classes/Main/MainViewController.swift
@@ -14,24 +14,34 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
         // 1.创建首页
         tabBar.tintColor = .orange
-        addChild(HomeTableViewController(), "首页", imageName: "tabbar_home")
-        addChild(MessageTableViewController(), "消息", imageName: "tabbar_message_center")
-        addChild(DiscoverTableViewController(), "发现", imageName: "tabbar_discover")
-        addChild(ProfileTableViewController(), "我", imageName: "tabbar_profile")
+        addChild("HomeTableViewController", "首页", imageName: "tabbar_home")
+        addChild("MessageTableViewController", "消息", imageName: "tabbar_message_center")
+        addChild("DiscoverTableViewController", "发现", imageName: "tabbar_discover")
+        addChild("ProfileTableViewController", "我", imageName: "tabbar_profile")
     }
     
     /**
      初始化自控制
      */
-    private func addChild(_ childController: UIViewController,_ title:String, imageName:String) {
-        // 1 设置首页tabbar 对应的数据
-        childController.tabBarItem.image = UIImage(named: imageName)
-        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-        childController.title = title
+    private func addChild(_ childControlleraName: String,_ title:String, imageName:String) {
         
+        // -1 动态获取命名空间
+        let ns = Bundle.main.infoDictionary!["CFBundleExecutable"] as! String
+        
+        // 0 讲字符串转换c为类
+        let cls : AnyClass? = NSClassFromString(ns + "." + childControlleraName)
+        // 0.1 创建对象 将 AnyClass 转换制定类型
+        let vcCls = cls as! UIViewController.Type
+        let vc = vcCls.init()
+        
+        // 1 设置首页tabbar 对应的数据
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        vc.title = title
+
         // 2.给首页包装导航控制器
         let nav = UINavigationController()
-        nav.addChild(childController)
+        nav.addChild(vc)
         // 3.add
         addChild(nav)
     }
