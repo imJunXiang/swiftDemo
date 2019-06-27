e949f9b2ce03088fb6150909a6c90e80241db3b8
俊祥 <909015430@qq.com>
2019-06-27 22:32:50 +0800

    完善菜单

--- JXWeibo/Classes/Home/HomeTableViewController.swift
+++ JXWeibo/Classes/Home/HomeTableViewController.swift
@@ -57,11 +57,70 @@ class HomeTableViewController: BaseTableViewController {
     @objc func rightItemClick() {
         print(#function)
     }
+    
+    // 记录当前书否是展开
+    var isPresent: Bool = false
 }
 
-extension HomeTableViewController : UIViewControllerTransitioningDelegate {
+extension HomeTableViewController : UIViewControllerTransitioningDelegate,UIViewControllerAnimatedTransitioning {
+    
     //  实现代理方法，告诉系统谁来负责转场动画
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         return PopoverPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+    
+    // 告诉系统谁来负责展现动画
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresent = true
+        return self
+    }
+    
+    // 告诉系统谁来负责消失动画
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresent = false
+        return self
+    }
+    
+    // MARK: - UIViewControllerAnimatedTransitioning
+    // MARK: - 只要实现这两个方法就没有默认动画了
+    // 返回动画时长  transitionContexts 上下文 返回了所有参数
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    // 告诉系统如何动画
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        if isPresent {
+            // 1.拿到展现的视图
+            let toView = transitionContext.view(forKey: .to)
+            toView?.transform = CGAffineTransform(scaleX: 1.0, y: 0.0)
+            
+            // 注意 一定要将视图添加到容器上
+            transitionContext.containerView.addSubview(toView!)
+            
+            // 设置锚点
+            toView?.layer.anchorPoint = CGPoint(x: 0.5, y: 0)
+            
+            // 2.执行动画
+            UIView.animate(withDuration: 0.5, animations: {
+                // 情况transform
+                toView?.transform = CGAffineTransform(translationX: 1, y: 1)
+            }) { (_) in
+                // 动画执行完毕 一定告知系统 不然可能出w未知错误
+                transitionContext.completeTransition(true)
+            }
+        } else {
+            // 关闭
+            let fromView = transitionContext.view(forKey: .from)
+            UIView.animate(withDuration: 0.2, animations: {
+                // 注意：由于CGFloat是不准确的 所以写0.0会没有动画
+                // 压扁
+                fromView?.transform = CGAffineTransform(scaleX: 1.0, y: 0.000001)
+            }) { (_) in
+                transitionContext.completeTransition(true)
+            }
+        }
+        
     }
 }
