71af105857f51dc18d3d50c96cf311e55457d05d
俊祥 <909015430@qq.com>
2019-06-28 23:23:46 +0800

    二维码下

--- JXWeibo/Classes/Home/QRCode/QRCodeViewController.swift
+++ JXWeibo/Classes/Home/QRCode/QRCodeViewController.swift
@@ -12,9 +12,12 @@ class QRCodeViewController: UIViewController {
     @IBAction func closeBtnClick(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+    // 底部视图
+    @IBOutlet weak var customTabbar: UITabBar!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 1.设置底部视图默认选择第0个
+        customTabbar.selectedItem = customTabbar.items![0]
     }
 
 }
