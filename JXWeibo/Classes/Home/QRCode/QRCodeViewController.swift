8b48136ef5d2a364341e8afc84e8c10d0e187381
俊祥 <909015430@qq.com>
2019-07-02 22:24:03 +0800

    二维码动画 storyBoard 写的，不好用

--- JXWeibo/Classes/Home/QRCode/QRCodeViewController.swift
+++ JXWeibo/Classes/Home/QRCode/QRCodeViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class QRCodeViewController: UIViewController {
+class QRCodeViewController: UIViewController,UITabBarDelegate {
+    // 扫描容器的高度
+    @IBOutlet weak var containerHeightCons: NSLayoutConstraint!
+    // 冲击波
+    @IBOutlet weak var scanlineCons: NSLayoutConstraint!
+    // 冲击波顶部约束
+    @IBOutlet weak var scanLineView: UIImageView!
     // 休息 storyBoard 是真不好用
     @IBAction func closeBtnClick(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -19,6 +25,35 @@ class QRCodeViewController: UIViewController {
         super.viewDidLoad()
         // 1.设置底部视图默认选择第0个
         customTabbar.selectedItem = customTabbar.items![0]
+        customTabbar.delegate = self
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 让约束重顶部开始
+        self.scanlineCons.constant = -self.containerHeightCons.constant
+        self.scanLineView.layoutIfNeeded()
+        UIView.setAnimationRepeatCount(MAXFLOAT)
+        // 执行冲击波动画
+        UIView.animate(withDuration: 5.0) {
+            // 1.修改约束
+            self.scanlineCons.constant = self.containerHeightCons.constant
+            
+            // 2.强制更新
+            self.scanLineView.layoutIfNeeded()
+        }
+    }
+    
+    //MARK: - UITabBarDelegate
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if item.tag == 1 {
+            print("二维码")
+            self.containerHeightCons.constant = 300
+        } else {
+            print("条形码")
+            self.containerHeightCons.constant = 150
+        }
+        
+        self.scanLineView.layer.removeAllAnimations()
+    }
 }
